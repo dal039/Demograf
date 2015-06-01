fe1281774a6e6db312a14ca42580999d54ccbcbe
Christian Memije <christianmemije@yahoo.com>
2015-06-01 02:58:50 -0700

    Added a slider

--- public/js/map.js
+++ public/js/map.js
@@ -101,3 +101,20 @@ function moveTooltip() {
 function hideTooltip() {
     tooltip.style("display", "none");
 }
+
+/////////////////////sliders/////////////////////
+$(function() {
+    $("#range").ionRangeSlider({
+        hide_min_max: true,
+        keyboard: true,
+        min: 0,
+        max: 5000,
+        from: 1000,
+        to: 4000,
+        type: 'double',
+        step: 1,
+        prefix: "$",
+        grid: true
+    });
+
+});
