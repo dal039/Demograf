0713b6525615684fd818ce320815f51c81583f55
Christian Memije <christianmemije@yahoo.com>
2015-06-01 19:42:47 -0700

    Refractored

---
+++ public/js/sliders.js
@@ -0,0 +1,64 @@
+/////////////////////sliders/////////////////////
+//Monthly Income
+$(function() {
+    $("#range1").ionRangeSlider({
+        type: "double",
+        min: 40000,
+        max: 130000,
+        step: 10000,
+        prettify_enabled: true,
+        prefix: "$",
+        grid: true,
+        grid_snap: true,
+        force_edges: true
+    });
+});
+
+//Home Value
+$(function() {
+    $("#range2").ionRangeSlider({
+        type: "double",
+        min: 190000,
+        max: 730000,
+
+        step: 10000,
+        prettify_enabled: true,
+        prefix: "$",
+
+        grid_snap: true,
+        force_edges: true
+    });
+});
+
+//Median Contract Rent
+$(function() {
+    $("#range3").ionRangeSlider({
+        type: "double",
+        min: 790,
+        max: 2000,
+
+        step: 100,
+        prettify_enabled: true,
+        prefix: "$",
+        max_postfix: "+",
+        grid: true,
+        grid_snap: true,
+        force_edges: true
+    });
+});
+
+
+//Employment status
+$(function() {
+    $("#range4").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 100,
+        step: 10,
+        prettify_enabled: true,
+        postfix: "%",
+        grid: true,
+        grid_snap: true,
+        force_edges: true,
+    });
+});
