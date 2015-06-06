753408f58b74b19f94c7e8aef4d927dbef0064c1
Kane Chong <khchong@ucsd.edu>
2015-06-05 19:33:44 -0700

    refactored files and tested onchange function

--- public/js/demograf.js
+++ public/js/demograf.js
@@ -1,3 +1,176 @@
+/*****************************************************************************************
+*************************************** SLIDERS ******************************************
+*****************************************************************************************/
+
+//Monthly Income
+$(function() {
+    $("#income").ionRangeSlider({
+        type: "double",
+        min: 40000,
+        max: 130000,
+        step: 10000,
+        prettify_enabled: true,
+        prefix: "$",
+        grid: true,
+        grid_snap: true,
+        force_edges: true,
+        onChange: function(data) {
+            console.log("INCOME CHANGED: " + data.to);
+        }
+    });
+});
+
+//Unemployment %
+$(function() {
+    $("#unemployed").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 100,
+        step: 10,
+        prettify_enabled: true,
+        postfix: "%",
+        grid: true,
+        grid_snap: true,
+        force_edges: true,
+    });
+});
+
+//snap %
+$(function() {
+    $("#snap").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 100,
+        step: 10,
+        prettify_enabled: true,
+        postfix: "%",
+        grid: true,
+        grid_snap: true,
+        force_edges: true,
+    });
+});
+
+//cash_assistance %
+$(function() {
+    $("#cash_assistance").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 100,
+        step: 10,
+        prettify_enabled: true,
+        postfix: "%",
+        grid: true,
+        grid_snap: true,
+        force_edges: true,
+    });
+});
+
+//Poverty %
+$(function() {
+    $("#poverty").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 100,
+        step: 10,
+        prettify_enabled: true,
+        postfix: "%",
+        grid: true,
+        grid_snap: true,
+        force_edges: true,
+    });
+});
+
+//At least a Bachelors  Degree %
+$(function() {
+    $("#range8").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 100,
+        step: 10,
+        prettify_enabled: true,
+        postfix: "%",
+        grid: true,
+        grid_snap: true,
+        force_edges: true,
+    });
+});
+
+//House value
+$(function() {
+    $("#house_value").ionRangeSlider({
+        type: "double",
+        min: 40000,
+        max: 130000,
+        step: 10000,
+        prettify_enabled: true,
+        prefix: "$",
+        grid: true,
+        grid_snap: true,
+        force_edges: true
+    });
+});
+
+//rent
+$(function() {
+    $("#rent").ionRangeSlider({
+        type: "double",
+        min: 40000,
+        max: 130000,
+        step: 10000,
+        prettify_enabled: true,
+        prefix: "$",
+        grid: true,
+        grid_snap: true,
+        force_edges: true
+    });
+});
+
+//Degree %
+$(function() {
+    $("#degree").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 100,
+        step: 10,
+        prettify_enabled: true,
+        postfix: "%",
+        grid: true,
+        grid_snap: true,
+        force_edges: true,
+    });
+});
+
+//Speak Other Language %
+$(function() {
+    $("#language").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 100,
+        step: 10,
+        prettify_enabled: true,
+        postfix: "%",
+        grid: true,
+        grid_snap: true,
+        force_edges: true,
+    });
+});
+
+//prints values
+$(function() {
+    $("#range8").on("change", function() {
+        var $this = $(this),
+            from = $this.data("from"),
+            to = $this.data("to");
+
+        console.log(from + " - " + to);
+    });
+});
+
+
+/*****************************************************************************************
+************************************ D3/GEOJSON MAP **************************************
+*****************************************************************************************/
+
 // regions info
 var regionInfo = { 
     "Carlsbad" : {},
@@ -50,7 +223,7 @@ var DelphiDemo = DelphiDemo || (function() {
      */
     self.getDelphiData = function() {
         
-        $.getJSON("/delphi_home_value", function(data) {
+        /*$.getJSON("/delphi_home_value", function(data) {
             console.log(data);
         });
 
@@ -104,23 +277,65 @@ var DelphiDemo = DelphiDemo || (function() {
 
         $.getJSON("/delphi_rental_statistics", function(data) {
             console.log(data);
-        });
+        });*/
 
     };
 
+    self.createMap = function() {
+        self.map = new Datamap({
+            element: document.getElementById('map'),
+            geographyConfig: {
+                dataUrl: 'data/regions.topo.json'
+            },
+            scope: 'regions',
+            responsive: true,
+            fills: {
+                defaultFill: '#212121'
+            },
+            setProjection: function(element) {
+                var projection = d3.geo.mercator()
+                    .center([-116.9, 32.9])
+                    .scale(27000)
+                    .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+                var path = d3.geo.path().projection(projection);
+                
+                return {
+                    path: path,
+                    projection: projection
+                };
+            },
+            done: function(datamap) {
+                datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
+                    console.log(geography.properties.name);
+                });
+            }
+        });
+
+    };
 
     /** 
      * initialize 
      */
     self.init = function() {
         self.getDelphiData();
+        self.createMap();
     };
 
     return self;
 })();
 
+
+/*****************************************************************************************
+************************************* ENTRY POINT ****************************************
+*****************************************************************************************/
+
 $(document).ready(function() {
 
     DelphiDemo.init();
 
+    $(window).on('resize', function() {
+        DelphiDemo.map.resize();
+    });
+
+    (DelphiDemo.map.svg[0])[0].id = "demograf-map";    
 });
